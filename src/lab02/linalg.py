c1d21e9877a9d406e6f84302b7f8af26cd360f0b
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-10-27 10:37:54 -0600

    added processing for shape of RHS in forward_substitution()

--- src/lab02/linalg.py
+++ src/lab02/linalg.py
@@ -22,7 +22,41 @@ def forward_substitution(L, b):
         This will have the same shape as b.
 
     """
+    # checking that input arguments can be converted to array of float
+    # also making a local copy of these arrays
     L = np.array(L, dtype=float)
     b = np.array(b, dtype=float)
 
-    return np.zeros_like(b)
+    # check shape of coefficient matrix L
+    L_shape = L.shape
+    if not len(L_shape) == 2:
+        raise ValueError(
+            f"Coefficient matrix L has shape {L_shape}, "
+            + f"len(L.shape) == {len(L_shape)}, must be 2."
+        )
+    n = L_shape[0]
+    if n != L_shape[1]:
+        raise ValueError(f"Coefficient matrix L has shape {L_shape}, must be square.")
+
+    # check shape of the right-hand-side b
+    b_shape = b.shape
+    if not (len(b_shape) == 1 or len(b_shape) == 2):
+        raise ValueError(
+            f"Right-hand-side vector b has shape {b_shape}, "
+            + f"len(b.shape) == {len(b_shape)}, must be 1 or 2."
+        )
+    if n != b_shape[0]:
+        raise ValueError(
+            f"Coefficient matrix L has {n} rows and cols, "
+            + f"right-hand-side b has {b_shape[0]} rows, "
+            + "dimensions must match."
+        )
+
+    # check for b_shape as 1d
+    if len(b_shape) == 1:
+        b = np.reshape(b, shape=(n, 1))
+
+    # form the augmented matrix
+    aug = np.hstack([L, b])
+
+    return np.reshape(aug[:, n:], shape=b_shape)
