a0ed6ad74995e179eb18853cbc947d3b5aa44fa0
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-10-27 09:12:11 -0600

    setup forward substitution test

---
+++ examples/tests.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+def main():
+    print("Hi! I am in main().")
+    print(f"np.__name__: {np.__name__}")
+
+    test_forward_substitution()
+
+
+def test_forward_substitution():
+    print("\nTesting forward substitution:")
+    # lower triangular coefficient matrix
+    A = np.array(
+        [
+            [
+                2135.0,
+                0.0,
+                0.0,
+                0.0,
+            ],
+            [
+                -2135.0,
+                5200.0,
+                0.0,
+                0.0,
+            ],
+            [
+                0.0,
+                -5200.0,
+                5796.0,
+                0.0,
+            ],
+            [
+                0.0,
+                0.0,
+                -5796.0,
+                7060.0,
+            ],
+        ]
+    )
+    print("A:")
+    print(A)
+
+    # right-hand side vector
+    b = np.array([500.0, 700.0, 1000.0, 500.0])
+    print("b:")
+    print(b)
+
+    # solve using numpy.linalg.solve()
+    x_exp = np.linalg.solve(A, b)
+    print("expected (numpy.linalg.solve):")
+    print(x_exp)
+
+
+if __name__ == "__main__":
+    main()
